032bd94621a8ba878d0ade5f33795756f2babb41
mystified131 <mystifiedthomas@gmail.com>
2018-12-04 08:30:18 -0600

    Getting started

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View(context.Categories.ToList());
+        }
+
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+    }
+}
